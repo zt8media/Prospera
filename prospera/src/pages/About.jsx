ca989c67e1200d3dd433cd0b91377b01f9105a2b
Ly My Y <lymy7724@gmail.com>
2024-08-28 14:47:56 -0400

    added about page
    added the headings in the about page

---
+++ prospera/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import Video from '../media/hero-video.mp4'
+
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div>
+      <Video/>
+    </div>
+    <h1>Who are we?</h1>
+    <p>Prospera is designed to equip young learners with essential money management skills and knowledge. We offer interactive lessons and activities tailored to pre-teens' learning styles and developmental needs. Prospera cover topics such as budgeting, saving, responsible spending, and basic investing concepts in an engaging and accessible manner. By incorporating real-life scenarios and relatable examples, these resources help bridge the gap between classroom learning and practical financial decision-making. </p>
+    </>
+  )
+}
