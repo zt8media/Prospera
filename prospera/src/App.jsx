2baebf9a9b226fca8642e3069af6f51efb6a5189
zt8media <zt8media@gmail.com>
2024-08-28 15:35:14 -0400

    chore: Remove Tailwind CSS integration and related configurations
    - Deleted Tailwind CSS files and configurations
    - Removed Tailwind CSS imports and dependencies
    - Reverted to using styled-components for styling

--- prospera/src/App.jsx
+++ prospera/src/App.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LearnPage from './pages/Learn'
 import GamePage from './pages/GamePage'
+import Navbar from './pages/NavBar';
 
 function App() {
 
   return (
 <>
-{/* <LearnPage/> */}
+
 <Router>
-    
+    <Navbar/>
      <Routes>
         <Route path="/" element={<LearnPage />} />
         <Route path="/game/:topic" element={<GamePage />} />
       </Routes>
     </Router>
+    
     </>
   )
 }
