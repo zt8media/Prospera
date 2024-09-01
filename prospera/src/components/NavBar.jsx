975993afef7d27acd0f399ad515cabc2b7e092d1
zt8media <zt8media@gmail.com>
2024-09-01 14:01:00 -0400

    Refactor LearnPage for Improved Responsiveness and Layout
    - Made ActivityPig logo and PageTitle responsive with proper alignment on smaller screens.
    - Adjusted ModalContent to ensure proper padding and visibility across all screen sizes.
    - Updated ModalRow and buttons to enhance layout in mobile view, ensuring all content is visible without horizontal scrolling.
    - Refined the overall styling to ensure a consistent user experience across desktop and mobile devices.

--- prospera/src/components/NavBar.jsx
+++ prospera/src/components/NavBar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/nav.css'
-import logo from '../media/user-solid.svg'
+import '../styles/nav.css';
+import logo from '../media/Navbar-pig.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
   const token = localStorage.getItem('token');
   let userRole = null;
 
@@ -15,19 +18,34 @@ const Navbar = () => {
     }
   }
 
+  const toggleMobileMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobile(false);
+  };
+
   return (
-    
-    <nav>
-      <div className='navlinks'>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/Learn">Learn</Link></li>
-      <li><Link to="/About">About Us</Link></li>
-      <li><Link to="/Contact">Contact</Link></li>
-      <li><Link to="/Login">Login</Link></li>
-      {userRole === 'admin' && <Link to="/Admin">Admin Dashboard</Link>}
-      <img src={logo} alt='user-profile-logo' className='user-logo'></img>
+    <nav className="navbar">
+      <div className="nav-logo-container">
+        <img src={logo} alt="Prospera Logo" className="nav-logo" />
+        <span className="nav-title">Prospera</span>
       </div>
-      
+      <ul className={`navlinks ${isMobile ? 'navlinks-mobile' : ''}`}>
+        <li><Link to="/" onClick={closeMobileMenu}>Home</Link></li>
+        <li><Link to="/Learn" onClick={closeMobileMenu}>Learn</Link></li>
+        <li><Link to="/About" onClick={closeMobileMenu}>About Us</Link></li>
+        <li><Link to="/Contact" onClick={closeMobileMenu}>Contact</Link></li>
+        <li><Link to="/Login" onClick={closeMobileMenu}>Login</Link></li>
+        {userRole === 'admin' && (
+          <li><Link to="/Admin" onClick={closeMobileMenu}>Admin Dashboard</Link></li>
+        )}
+    
+      </ul>
+      <div className="hamburger" onClick={toggleMobileMenu}>
+        <FontAwesomeIcon icon={isMobile ? faTimes : faBars} />
+      </div>
     </nav>
   );
 };
